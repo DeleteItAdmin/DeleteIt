608310e0e7ea97a7b0745db4715bbaf1ec0bd300
chetnak <chetnak@chetnas-mbp>
2018-04-21 18:35:15 +0530

    Angular Node Sample

---
+++ node-todo-tut1-starter/config/database.js
@@ -0,0 +1,8 @@
+//module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+//}
+module.exports = {
+    'url' : 'mongodb://localhost:27017/my-test-db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
